5fdf5f0037ec23b2e44811a61ac0788b28f5ed25
Brad <brad@brad.tw>
2019-08-01 19:43:57 +0800

    05

--- app/src/main/java/tw/org/tcca/app/posttest/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/posttest/MainActivity.java
@@ -1,12 +1,18 @@
 package tw.org.tcca.app.posttest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -20,11 +26,14 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private EditText cname, tel, addr;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
 
         cname = findViewById(R.id.inputCName);
         tel = findViewById(R.id.inputTel);
@@ -52,9 +61,9 @@ public class MainActivity extends AppCompatActivity {
             conn.setDoOutput(true);
 
             ContentValues values = new ContentValues();
-            values.put("cname", "brad123");
-            values.put("tel", "9487945123");
-            values.put("addr", "abcdefg");
+            values.put("cname", cname.getText().toString());
+            values.put("tel", tel.getText().toString());
+            values.put("addr", addr.getText().toString());
             String query = queryString(values);
             Log.v("brad", query);
 
@@ -74,16 +83,32 @@ public class MainActivity extends AppCompatActivity {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 String json = reader.readLine();
                 Log.v("brad", "json:" + json);
+                parseAddJSON(json);
             }
-
-
-
-
 
         }catch (Exception e){
 
         }
     }
+
+    private void parseAddJSON(String json){
+        try {
+            JSONObject root = new JSONObject(json);
+            int status = root.getInt("status");
+            handler.sendEmptyMessage(status);
+        }catch (Exception e){
+            handler.sendEmptyMessage(-1);
+        }
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Toast.makeText(MainActivity.this, msg.what==0?"Add Success!":"Add Failure!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     private String queryString(ContentValues data){
         Set<String> keys = data.keySet();
